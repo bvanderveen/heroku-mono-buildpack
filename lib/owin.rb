74305998fd6472aa366c6fa402169b35b3b6f4d6
Benjamin van der Veen <b@bvanderveen.com>
2011-12-10 20:42:15 -0800

    maybe fix compile

--- lib/owin.rb
+++ lib/owin.rb
@@ -16,7 +16,7 @@ module Owin
 		mono_binaries_url = "https://cloudant.com/db/bvanderveen/make/23399466-c06c-4c94-ae84-b9e040a6a38f/output"
 		mono_path = File.join(build_dir, "_mono")
 
-		Dir.mkdir_p(mono_path)
+		FileUtils.mkdir_p(mono_path)
 		Dir.chdir(mono_path) do
 			run("curl #{mono_binaries_url} -o - | tar xzf -")
 		end
