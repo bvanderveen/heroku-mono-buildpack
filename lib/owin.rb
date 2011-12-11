7787b45368c7ad40705d185f0fc5a8befec8dbcb
Benjamin van der Veen <b@bvanderveen.com>
2011-12-10 20:41:00 -0800

    maybe fix compile

--- lib/owin.rb
+++ lib/owin.rb
@@ -7,7 +7,6 @@ end
 
 module Owin
 	def self.detect(build_dir)
-		puts '-----> attempting to detect mono'
 		Dir.chdir(build_dir)
 
 		return File.exists? "usemono"
@@ -17,6 +16,7 @@ module Owin
 		mono_binaries_url = "https://cloudant.com/db/bvanderveen/make/23399466-c06c-4c94-ae84-b9e040a6a38f/output"
 		mono_path = File.join(build_dir, "_mono")
 
+		Dir.mkdir_p(mono_path)
 		Dir.chdir(mono_path) do
 			run("curl #{mono_binaries_url} -o - | tar xzf -")
 		end
